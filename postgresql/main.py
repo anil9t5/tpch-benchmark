d96fe3f7fd8ef8911ce3bdd581974ea63b411865
SherryK7 <skhosra1@unb.ca>
2021-04-04 13:59:02 -0400

    Fixed gitignore and main

--- postgresql/main.py
+++ postgresql/main.py
@@ -5,14 +5,12 @@ from postgresql.run_queries import RunQueries
 
 def main():
     print("PostgreSQL db started...")
-    create_tables = False
-    if create_tables:
-        task = Tables()
-        task.create_tables()
-        insert_task = InsertData(0.05)
-        insert_task.insert_to_tables()
-    else:
-        query= RunQueries()
-        query.run_queries()
+    task = Tables()
+    task.create_tables()
+    insert_task = InsertData(0.05)
+    insert_task.insert_to_tables()
+
+    query = RunQueries()
+    query.run_queries()
 
     print("Successful!")
