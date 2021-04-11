a0d2c120ea1bc5cd26a3af4378ce33a06c7ee1d3
SherryK7 <skhosra1@unb.ca>
2021-04-11 14:31:14 -0400

    Initialization

---
+++ neo4jp/main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import sys
+from neo4jp.initialize_db import InitilizeDB
+
+def main():
+    print("Neo4j db started...")
+    db = InitilizeDB.init()
+    session = db.session()
+    session
+    session.close()
+    print("Successful!")
+
