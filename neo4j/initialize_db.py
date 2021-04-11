4ecfe2b023cfecfcc224117c745a8d9dfda96080
SherryK7 <skhosra1@unb.ca>
2021-04-11 15:17:44 -0400

    Initialization

--- neo4j/initialize_db.py
+++
@@ -1,8 +0,0 @@
-import py2neo
-from neo4j import GraphDatabase, basic_auth
-
-
-class InitilizeDB(object):
-    URI = "bolt://localhost:7687"
-    DRIVER = GraphDatabase.driver(URI, auth=("neo4j", "1"))
-    session = driver.session()
--- neo4j/query2.py
+++
@@ -1,7 +0,0 @@
-MATCH (p:PARTSUPP)-[:SUPPLIED_BY]->(s:SUPPLIER)-[:BELONG_TO]->(n:NATION)-[:IS_FROM]->(r:REGION)
-where r.R_NAME = 'EUROPE' with p, min(p.PS_SUPPLYCOST) as minvalue
-MATCH (ps:PARTSUPP)-[:COMPOSED_BY]->(p:PART)
-MATCH (ps:PARTSUPP)-[:SUPPLIED_BY]->(s:SUPPLIER)-[:BELONG_TO]->(n:NATION)-[:IS_FROM]->(r:REGION)
-where p.P_SIZE = 15 and p.P_TYPE =~ '.*BRASS.*' and r.R_NAME = 'EUROPE' and p.PS_SUPPLYCOST = minvalue
-return p.id,p.P_MFGR,s.S_ACCTBAL,s.S_NAME,s.S_ADDRESS,s.S_PHONE,s.S_COMMENT,n.N_NAME
-order by s.S_ACCTBAL desc,n.N_NAME,s.S_NAME,p.id;
