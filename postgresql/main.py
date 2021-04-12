16d88420fe2182340c80c83f9d63656450775d44
SherryK7 <skhosra1@unb.ca>
2021-04-12 14:39:04 -0400

    Insert from csv to customer and nation

--- postgresql/main.py
+++ postgresql/main.py
@@ -1,16 +1,29 @@
 # -*- coding: utf-8 -*-
 from postgresql.tables import Tables
 from postgresql.insert_data import InsertData
+from postgresql.insert_data_csv import InsertDataCsv
 from postgresql.run_queries import RunQueries
 
 def main():
     print("PostgreSQL db started...")
-    task = Tables()
-    task.create_tables()
-    insert_task = InsertData(0.05)
-    insert_task.insert_to_tables()
+    create_table=True
+    if create_table:
+        task = Tables()
+        task.create_tables()
 
-    query = RunQueries()
-    query.run_queries()
+    insert_data =True
+    read_from_csv=True
+    if insert_data:
+        if read_from_csv:
+            insert_task=InsertDataCsv(1)
+            insert_task.insert_to_tables()
+        else:
+            insert_task = InsertData(0.05)
+            insert_task.insert_to_tables()
+
+    run_queries=False
+    if run_queries:
+        query = RunQueries()
+        query.run_queries()
 
     print("Successful!")
