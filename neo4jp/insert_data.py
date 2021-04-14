334c6f3ee0c3842a7d9ac26ac26a88a0830a7610
SherryK7 <skhosra1@unb.ca>
2021-04-14 10:20:27 -0400

    Insert relations

--- neo4jp/insert_data.py
+++ neo4jp/insert_data.py
@@ -41,21 +41,50 @@ class InsertData:
             'customer.C_ACCTBAL = toFloat(line.C_ACCTBAL), '
             'customer.C_MKTSEGMENT = line.C_MKTSEGMENT, '
             'customer.C_COMMENT = line.C_COMMENT; ')
-
+#=======================================================
     @staticmethod
     def insert_relation_customer_nation(self):
         graphDB = InitilizeDB.init()
 
         graphDB.run(
-            'LOAD CSV WITH HEADERS FROM "file:///custnationrelation.csv" AS row FIELDTERMINATOR "|"'
+            'USING PERIODIC COMMIT'
+            'LOAD CSV WITH HEADERS FROM "file:///rel_customer_nation.csv" AS row FIELDTERMINATOR "|"'
             'MATCH (customer:CUSTOMER {id: toInteger(row.C_CUSTKEY)})'
             'MATCH (nation:NATION {id: toInteger(row.C_NATIONKEY)})'
             'MERGE (customer)-[:FROM_4]->(nation);')
 
+    @staticmethod
+    def insert_relation_lineitem_orders(self):
+        graphDB = InitilizeDB.init()
+
+        graphDB.run(
+            'USING PERIODIC COMMIT '
+            'LOAD CSV WITH HEADERS FROM "file:///rel_lineitem_orders.csv" AS row FIELDTERMINATOR "|" ' 
+            'MATCH (lineitem:LINEITEM {L_ORDERKEY: toInteger(row.L_ORDERKEY), L_PARTKEY:' 'toInteger(row.L_PARTKEY), L_SUPPKEY: toInteger(row.L_SUPPKEY),L_LINENUMBER:' 'toInteger(row.L_LINENUMBER) }) '
+            'MATCH (orders:ORDER {id: toInteger(row.L_ORDERKEY), O_CUSTKEY: toInteger(row.O_CUSTKEY)})'
+            'MERGE (lineitem)-[:BELONGS_TO_7]->(orders);')
+
+    @staticmethod
+    def insert_relation_lineitem_part(self):
+        graphDB = InitilizeDB.init()
+
+        graphDB.run(
+            'USING PERIODIC COMMIT '
+            'LOAD CSV WITH HEADERS FROM "file:///rel_lineitem_part.csv" AS row FIELDTERMINATOR "|" '
+            'MATCH (lineitem:LINEITEM {L_ORDERKEY: toInteger(row.L_ORDERKEY), L_PARTKEY: ' 
+            'toInteger(row.L_PARTKEY), L_SUPPKEY: toInteger(row.L_SUPPKEY),L_LINENUMBER: ' 
+            'toInteger(row.L_LINENUMBER) }) '
+            'MATCH (part:PART {id: toInteger(row.L_PARTKEY), P_NAME: row.P_NAME }) '
+            'MERGE (lineitem)-[:COMPOSED_BY_8]->(part); ')
+
     def insert_nodes(self):
         #InsertData.insert_nation_nodes(self)
         #InsertData.insert_customer_nodes(self)
-        pass
+        InsertData.insert_customer_nodes(self)
+        InsertData.insert_customer_nodes(self)
+        InsertData.insert_customer_nodes(self)
+
+
 
     def insert_relations(self):
         InsertData.insert_relation_customer_nation(self)
