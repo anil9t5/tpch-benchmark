14de8c622254a357d685b44b8946066824944873
Anil Hitang <anilhitang@Anils-MBP.phub.net.cable.rogers.com>
2021-04-18 08:12:35 -0300

    fixes on neo and postgres

--- neo4jp/query3.py
+++ neo4jp/query3.py
@@ -8,10 +8,10 @@ class Query3:
 
     def execute(self):
         try:
-            graphDB= InitilizeDB.init()
+            graphDB = InitilizeDB.init()
             start_time = time.time()
 
-            result =graphDB.run(
+            result = graphDB.run(
                 "MATCH (lineitem:LINEITEM)-[:IS_PART_OF]->(order:ORDER)-[:MADE_BY]->(customer:CUSTOMER) "
                 "WHERE customer.C_MKTSEGMENT = 'BUILDING' AND date(order.O_ORDERDATE) < date('1995-03-15') AND date(lineitem.L_SHIPDATE) > date('1995-03-15') "
                 "RETURN order.id, sum(lineitem.L_EXTENDEDPRICE*(1-lineitem.L_DISCOUNT)) AS REVENUE, order.O_ORDERDATE, order.O_SHIPPRIORITY "
@@ -26,4 +26,3 @@ class Query3:
             print("In seconds: " + str("{:.7f}".format(end_time - start_time)))
         except:
             print("py2neo ERROR:")
-
