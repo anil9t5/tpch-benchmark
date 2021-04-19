96d1a0d9187cd067948d906fa188ed112684e0a0
SherryK7 <skhosra1@unb.ca>
2021-04-19 07:10:56 -0400

    Printing results

--- neo4jp/query4.py
+++ neo4jp/query4.py
@@ -11,14 +11,14 @@ class Query4:
             graphDB = InitilizeDB.init()
             start_time = time.time()
 
-            graphDB.run(
+            result=graphDB.run(
                 "WITH date('1993-07-01') + duration('P3M') AS my_date "
                 "MATCH (lineitem: LINEITEM)-[:IS_PART_OF]->(order: ORDER) "
                 "WHERE  date(lineitem.L_COMMITDATE) < date(lineitem.L_RECEIPTDATE) AND date(order.O_ORDERDATE) >= date('1993-07-01') AND date(order.O_ORDERDATE) < date(my_date) "
                 "RETURN order.O_ORDERPRIORITY, COUNT(*) AS ORDER_COUNT "
                 "ORDER BY order.O_ORDERPRIORITY; ")
 
-            # print(list(result))
+            print(result)
             end_time = time.time()
             print("---------------Query 4-------------")
             print("Start time: " + str(start_time))
